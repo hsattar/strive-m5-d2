8050e97b934ad2bdcdd410c098329df415c00562
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-30 14:28:51 +0000

    added post ability

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,14 +1,15 @@
 import express from 'express'
-import fs, { readFileSync } from 'fs'
+import fs from 'fs'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
+import { v4 as uuidv4 } from 'uuid'
 
 const authorRoutes = express.Router()
 
 const currentFilePath = fileURLToPath(import.meta.url)
 const currentFolder = dirname(currentFilePath)
 const authorsJSON = join(currentFolder, 'authors.json')
-const authors = JSON.parse(readFileSync(authorsJSON))
+const authors = JSON.parse(fs.readFileSync(authorsJSON))
 
 authorRoutes.get('/', (req, res) => {
     res.send(authors)
@@ -21,7 +22,11 @@ authorRoutes.get('/:authorId', (req, res) => {
 })
 
 authorRoutes.post('/', (req, res) => {
-    res.send('post')    
+    const receivedInput = req.body
+    const newUser = {...receivedInput, id: uuidv4()}
+    const updatedAuthors = authors.push(newUser)
+    fs.writeFileSync(authorsJSON, JSON.stringify(updatedAuthors))
+    res.status(201).send(newUser)    
 })
 
 authorRoutes.put('/:authorId', (req, res) => {
