25ef9eb690c056decd2d00e6b90ef880c3da2135
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-30 19:34:54 +0000

    improved code grouping by routes

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -10,52 +10,65 @@ const currentFilePath = fileURLToPath(import.meta.url)
 const currentFolder = dirname(currentFilePath)
 const authorsJSON = join(currentFolder, 'authors.json')
 
-authorRoutes.get('/', (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSON))
-    res.send(authors)
-})
+// DEFAULT ROUTE - GET, POST
+authorRoutes.route('/')
+    .get((req, res) => {
+        const authors = JSON.parse(fs.readFileSync(authorsJSON))
+        res.send(authors)
+    })
+    .post((req, res) => {
+        const authors = JSON.parse(fs.readFileSync(authorsJSON))
+        const emailExists = authors.some(author => author.email === req.body.email)
+        if (emailExists) return res.status(400).send('A user with this email already exists')
+        const newUser = {
+            id: uuidv4(), 
+            ...req.body, 
+            avatar: `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}`,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        authors.push(newUser)
+        fs.writeFileSync(authorsJSON, JSON.stringify(authors))
+        res.status(201).send(newUser)    
+    })
 
-authorRoutes.get('/:authorId', (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSON))
-    const authorId = req.params.authorId
-    const singleAuthor = authors.find(author => author.id === authorId)
-    res.send(singleAuthor)
-})
 
-authorRoutes.post('/', (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSON))
-    const emailExists = authors.some(author => author.email === req.body.email)
-    if (emailExists) return res.status(400).send('A user with this email already exists')
+// SPECIFIC AUTHOR ROUTE - GET, PUT, DELETE
+authorRoutes.route('/:authorId')
+    .get((req, res) => {
+        const authors = JSON.parse(fs.readFileSync(authorsJSON))
+        const authorId = req.params.authorId
+        const singleAuthor = authors.find(author => author.id === authorId)
+        res.send(singleAuthor)
+    })
+    .put((req, res) => {
+        const authors = JSON.parse(fs.readFileSync(authorsJSON))
+        const authorId = req.params.authorId
+        const index = authors.findIndex(author => author.id === authorId)
+        authors[index] = {
+            ...authors[index], 
+            ...req.body,
+            avatar: `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}`,
+            updatedAt: new Date()
+        }
+        const updatedDetails = authors[index]
+        fs.writeFileSync(authorsJSON, JSON.stringify(authors))
+        res.send(updatedDetails)
+    })
+    .delete((req, res) => {
+        const authors = JSON.parse(fs.readFileSync(authorsJSON))
+        const authorId = req.params.authorId
+        const remainingAuthors = authors.filter(author => author.id !== authorId)
+        fs.writeFileSync(authorsJSON, JSON.stringify(remainingAuthors))
+        res.status(204).send()
+    })
     
-    const newUser = {...req.body, id: uuidv4()}
-    authors.push(newUser)
-    fs.writeFileSync(authorsJSON, JSON.stringify(authors))
-    res.status(201).send(newUser)    
-})
 
 authorRoutes.post('/checkEmail', (req, res) => {
     const authors = JSON.parse(fs.readFileSync(authorsJSON))
     const email = req.body.email
     const emailExists = authors.some(author => author.email === email)
     res.send(emailExists)
-})
-
-authorRoutes.put('/:authorId', (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSON))
-    const authorId = req.params.authorId
-    const index = authors.findIndex(author => author.id === authorId)
-    authors[index] = {...authors[index], ...req.body}
-    const updatedDetails = authors[index]
-    fs.writeFileSync(authorsJSON, JSON.stringify(authors))
-    res.send(updatedDetails)
-})
-
-authorRoutes.delete('/:authorId', (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSON))
-    const authorId = req.params.authorId
-    const remainingAuthors = authors.filter(author => author.id !== authorId)
-    fs.writeFileSync(authorsJSON, JSON.stringify(remainingAuthors))
-    res.status(204).send()
 })
 
 export default authorRoutes
