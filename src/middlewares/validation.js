0965917773917c0a6d506e3e11ddaca72d1de812
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-01 15:09:17 +0000

    added validation

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -4,5 +4,9 @@ export const blogsBodyValidator = [
     body('category').exists().withMessage('You Must Include A Category'),
     body('title').exists().withMessage('You Must Include A Title'),
     body('cover').exists().withMessage('You Must Include A Cover Image Link'),
-    body('content').exists().withMessage('You Must Include Some Content'),
+    body('readTime.value').exists().withMessage('You Must Include A Read Time Value'),
+    body('readTime.unit').exists().withMessage('You Must Include A Read Time Unit'),
+    body('author.name').exists().withMessage('You Must Include An Author Name'),
+    body('author.avatar').exists().withMessage('You Must Include An Author Avatar'),
+    body('content').exists().withMessage('You Must Include Some Content')
 ]
