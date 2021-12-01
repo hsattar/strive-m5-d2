1eec8b99726a98e0783f59b1361de0bee98989ae
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-01 16:02:53 +0000

    added min length validation

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,12 +1,12 @@
 import { body } from 'express-validator'
 
 export const blogsBodyValidator = [
-    body('category').exists().withMessage('You Must Include A Category'),
-    body('title').exists().withMessage('You Must Include A Title'),
-    body('cover').exists().withMessage('You Must Include A Cover Image Link'),
-    body('readTime.value').exists().withMessage('You Must Include A Read Time Value'),
-    body('readTime.unit').exists().withMessage('You Must Include A Read Time Unit'),
-    body('author.name').exists().withMessage('You Must Include An Author Name'),
-    body('author.avatar').exists().withMessage('You Must Include An Author Avatar'),
-    body('content').exists().withMessage('You Must Include Some Content')
+    body('category').exists().isLength({min: 1}).withMessage('You Must Include A Category'),
+    body('title').exists().isLength({min: 1}).withMessage('You Must Include A Title'),
+    body('cover').exists().isLength({min: 1}).withMessage('You Must Include A Cover Image Link'),
+    body('readTime.value').exists().isLength({min: 1}).withMessage('You Must Include A Read Time Value'),
+    body('readTime.unit').exists().isLength({min: 1}).withMessage('You Must Include A Read Time Unit'),
+    body('author.name').exists().isLength({min: 1}).withMessage('You Must Include An Author Name'),
+    body('author.avatar').exists().isLength({min: 1}).withMessage('You Must Include An Author Avatar'),
+    body('content').exists().isLength({min: 1}).withMessage('You Must Include Some Content')
 ]
