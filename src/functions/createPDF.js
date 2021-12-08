18b25a8b5c994283b6836bc678373b0ca5ee252f
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-08 15:55:43 +0000

    removed functio ito convert imageurl

--- src/functions/createPDF.js
+++ src/functions/createPDF.js
@@ -2,27 +2,6 @@ import PdfPrinter from 'pdfmake'
 
 const getPDFReadableStream = blog => {
 
-    function getBase64Image(img) {
-        // Create an empty canvas element
-        var canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-    
-        // Copy the image contents to the canvas
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-    
-        // Get the data-URL formatted image
-        // Firefox supports PNG and JPEG. You could check img.src to
-        // guess the original format, but be aware the using "image/jpg"
-        // will re-encode the image.
-        var dataURL = canvas.toDataURL("image/png");
-    
-        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-    }
-
-    const blogImage = getBase64Image(blog.cover)
-
     const fonts = {
         Helvetica: {
             normal: 'Helvetica',
@@ -38,7 +17,6 @@ const getPDFReadableStream = blog => {
             author: `${blog.author.name}`
         },
         content: [
-            { image: blogImage,  margin: [5, 50], fit: [450, 450] },
             { text: `${blog.title}`, style: 'header', margin: [0, 20] },
             `${blog.content}`
         ],
