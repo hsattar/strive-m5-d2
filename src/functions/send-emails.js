8f06dbeacc6d5951220e48b249ec899eb9f17ad8
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-09 15:39:39 +0000

    able to send pdf in email

--- src/functions/send-emails.js
+++ src/functions/send-emails.js
@@ -1,4 +1,5 @@
 import sgMail from '@sendgrid/mail'
+import fs from 'fs'
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
@@ -20,22 +21,39 @@ export const sendTestEmail = async data => {
 
 export const sendNewBlogCreatedEmail = async (email, blog) => {
 
-    const msg = {
-        to: `${email}`,
-        from: `${process.env.SENDER_EMAIL}`,
-        subject: `New Blog - ${blog.title}`,
-        text: `${blog.content}`,
-        html: `
-            <img src=${blog.cover} />
-            <h2>${blog.title}</h2>
-            <p>${blog.content}</p>
-            <a href=\`https://hs-strive-blogs.vercel.app/blog/${blog.id}\`>View The Blog</a>
-            `
-    }
+    fs.readFile(('./src/data/test.pdf'), async (err, data) => {
+        if (err) console.log(err)
+        if (data) {
+            const msg = {
+                to: `${email}`,
+                from: `${process.env.SENDER_EMAIL}`,
+                subject: `New Blog - ${blog.title}`,
+                text: `${blog.content}`,
+                html: `
+                    <img src=${blog.cover} />
+                    <h2>${blog.title}</h2>
+                    <p>${blog.content}</p>
+                    <a href=\`https://hs-strive-blogs.vercel.app/blog/${blog.id}\`>View The Blog</a>
+                    `,
+                attachments: [
+                    {
+                        content: data.toString('base64'),
+                        filename: `${blog.title}.pdf`,
+                        type: 'application/csv',
+                        disposition: 'attachment',
+                        content_id: blog.id
+                    }
+                ]
+            }
 
-    try {
-        await sgMail.send(msg)    
-    } catch (error) {
-        console.log(error)
-    }
+            try {
+                await sgMail.send(msg)    
+            } catch (error) {
+                console.log(error)
+            }
+
+        }
+    })
+        
+
 }
